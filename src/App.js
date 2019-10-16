7d95035592a8b6420cfb26191a89fe1eaac1bb21
pascualbeatriz <pascuals.beatriz@gmail.com>
2019-10-16 13:40:21 +0200

    Fetch Api petition

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pokemons:[]
+    };
+  }
+
+  componentDidMount(){
+    this.getPokemons();
+  }
+
+  getPokemons(){
+    const getUrl= "https://pokeapi.co/api/v2/pokemon?limit=25";
+
+    fetch(getUrl)
+      .then(response => response.json())
+        .then(data => {
+           for(let pokemonList of data.results)
+    fetch(pokemonList.url)
+      .then(response => response.json())
+        .then(pokemonData =>{
+          
+        })  
+    });
+  }
+
+
+  render() {
+    console.log('me estoy pintando');
+    const {pokemons} = this.state;
+      return (
+        <div className="App">
+        <ul className="pokemon__list">
+          
+          
+        </ul>
+        </div>
+      );
+    }
 }
 
 export default App;
